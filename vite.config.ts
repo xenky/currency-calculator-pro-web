4fc98d06ac229301c62cf552881fd775ea90fc1e
Freddy Rujano <freddyrujanomora@gmail.com>
2025-07-02 06:00:29 -0400

    update service-worker

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig, loadEnv } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 import { fileURLToPath, URL } from 'url';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       base: '/currency-calculator-pro-web/',
+      plugins: [
+        tailwindcss(),
+      ],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
