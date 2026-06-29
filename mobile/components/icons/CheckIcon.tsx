d7d649997ea9192e440760354f8cc91c6979e159
Freddy Rujano <freddyrujanomora@gmail.com>
2026-06-29 18:34:27 -0400

    ?

--- mobile/components/icons/CheckIcon.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import { Svg, Path } from 'react-native-svg';
-
-export const CheckIcon: React.FC<any> = (props) => (
-  <Svg fill="none" viewBox="0 0 24 24" strokeWidth={3} stroke="currentColor" {...props}>
-    <Path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
-  </Svg>
-);
